3a27dc16221e2b83f165c8c6c34478a72f43c297
jguerrab <jguerrab@unal.edu.co>
2024-03-31 18:57:51 -0500

    Update pregunta_11.sql

--- pregunta_11.sql
+++ pregunta_11.sql
@@ -38,6 +38,7 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
-SELECT strftime('%Y', c14) AS YEAR, COUNT(*) AS "COUNT(*)"
+SELECT COUNT(strftime('%Y', c14)) 
+AS "COUNT(*)"
 FROM tbl1
 WHERE strftime('%Y', c14)="2018";
